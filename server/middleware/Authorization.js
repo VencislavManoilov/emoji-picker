1fe16aa621890bd6cdb0371a192b5ebf9e7438dd
VencislavManoilov <vencislav2.manoilov@gmail.com>
2025-04-13 19:40:46 +0300

    😅 Fixed the authorization

--- server/middleware/Authorization.js
+++ server/middleware/Authorization.js
@@ -1,11 +1,9 @@
 function Authorization(req, res, next) {
-  // Check if the user is authenticated
-  if (req.token === process.env.TOKEN) {
-    // User is authenticated, proceed to the next middleware or route handler
-    return next();
-  }
+    const token = req.headers['authorization'];
+    if (token === process.env.TOKEN) {
+        return next();
+    }
 
-  // User is not authenticated, redirect to the login page
   res.status(401).json({ error: 'Unauthorized access. Please log in' });
 }
 
